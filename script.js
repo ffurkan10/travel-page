dcf576b7e6d5ba0a400560fcac27e95ef51f4c4b
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2022-04-30 22:27:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,25 +14,25 @@ closeMenu.addEventListener("click", () => {
   hamburger.classList.toggle("burger-click");
 });
 
-ScrollReveal().reveal(".ani1", { delay: 250 });
-ScrollReveal().reveal(".ani2", { delay: 500 });
-ScrollReveal().reveal(".ani3", { delay: 750 });
-ScrollReveal().reveal(".ani4", { delay: 1000 });
-ScrollReveal().reveal(".ani5", { delay: 1250 });
-ScrollReveal().reveal(".ani6", { delay: 1500 });
-ScrollReveal().reveal(".ani7", { delay: 1750 });
-ScrollReveal().reveal(".ani8", { delay: 2000 });
+ScrollReveal().reveal(".ani1", { delay: 150 });
+ScrollReveal().reveal(".ani2", { delay: 300 });
+ScrollReveal().reveal(".ani3", { delay: 450 });
+ScrollReveal().reveal(".ani4", { delay: 600 });
+ScrollReveal().reveal(".ani5", { delay: 750 });
+ScrollReveal().reveal(".ani6", { delay: 900 });
+ScrollReveal().reveal(".ani7", { delay: 1050 });
+ScrollReveal().reveal(".ani8", { delay: 1200 });
 
 window.sr = ScrollReveal();
 
 sr.reveal(".anime-left", {
-  origin: "left",
+  origin: "top",
   duration: 1000,
   distance: "25rem",
-  delay: 300,
+  delay: 150,
 });
 sr.reveal(".anime-right", {
-  origin: "right",
+  origin: "top",
   duration: 1000,
   distance: "25rem",
   delay: 500,
